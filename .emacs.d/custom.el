bfe1570bd5391ea4463b34a06c342564a8491327
sylvain.ribstein <sylvain.ribstein@movesol.com>
2019-03-07 13:47:05 +0100

    add gnus

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -9,12 +9,12 @@
       ((org-agenda-tag-filter-preset
         (quote
          ("-volim" "-hidden"))))))))
- ;; '(package-selected-packages
- ;;   (quote
- ;;    (expand-region pdf-tools smartparens flycheck nnir ivy-rich ivy-pass counsel wiki-summary which-key undo-tree rainbow-mode paradox move-text flyspell-correct-ivy dashboard engine-mode w3m utop use-package-ensure-system-package tuareg switch-window switch-buffer-functions smex smart-mode-line ranger rainbow-delimiters org-gcal ocp-indent nord-theme merlin markdown-mode major-mode-hydra magit iedit helm-descbinds groovy-mode flymd eclim delight csv-mode company-math company-auctex cobol-mode calfw-org calfw auto-complete async-await ascii-art-to-unicode alert-termux aggressive-indent ace-window))))
- ;; (custom-set-faces
- ;;  ;; custom-set-faces was added by Custom.
- ;;  ;; If you edit it by hand, you could mess it up, so be careful.
- ;;  ;; Your init file should contain only one such instance.
- ;;  ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (toc-org wiki-summary which-key w3m utop use-package-ensure-system-package undo-tree tuareg switch-window switch-buffer-functions smex smartparens smart-mode-line ranger rainbow-mode rainbow-delimiters pdf-tools paradox org-gcal ocp-indent nord-theme move-text merlin markdown-mode major-mode-hydra magit-popup magit ivy-rich ivy-pass iedit helm-descbinds groovy-mode flyspell-correct-ivy flymd flycheck expand-region engine-mode eclim delight dashboard csv-mode counsel company-math company-auctex cobol-mode calfw-org calfw auto-complete async-await ascii-art-to-unicode alert-termux aggressive-indent ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  )
--- .gnus.el
+++ .gnus.el
@@ -1,1 +1,191 @@
-Dropbox/.gnus.el
+;;ask encryption password once
+(setq epa-file-cache-passphrase-for-symmetric-encryption t)
+
+(setq
+ gnus-select-method
+ '(nnimap "gmail"
+          (nnimap-address "imap.gmail.com")
+          (nnimap-server-port "imaps")
+          (nnimap-stream ssl)
+          (nnir-search-engine imap)
+          (nnmail-expiry-target "[Gmail]/Papelera")
+          (nnmail-expiry-wait immediate)))
+
+(setq
+ gnus-secondary-select-methods
+ '((nnimap "outlook"
+           (nnimap-address "outlook.office365.com")
+           (nnimap-server-port 993)
+           (nnimap-stream ssl)
+           (nnir-search-engine imap)
+           (nnmail-expiry-target "nnimap+outlook:Junk")
+           (nnmail-expiry-wait immediate))
+   ;; (nnml   "solutec"
+   ;;         ((pop :server "127.0.0.1" ;; DavMail is running atlocalhost
+   ;;               :stream network))) ;; by default, DavMail  don't encrypt mail
+   ))
+
+;; gnus+davmail bug, so I have to use pop3 for DavMail
+;; http://permalink.gmane.org/gmane.emacs.gnus.general/83301
+;; but delete all the mails on server is scary
+(setq pop3-leave-mail-on-server t)
+
+
+(setq message-send-mail-function 'smtpmail-send-it
+      smtpmail-default-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-service 587
+      gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]"
+      smtpmail-local-domain "homepc")
+
+(setq gnus-thread-sort-functions
+      '(gnus-thread-sort-by-most-recent-date
+        (not gnus-thread-sort-by-number)))
+
+
+;; NO 'passive
+(setq gnus-use-cache t)
+
+;; Fetch only part of the article if we can.
+;; I saw this in someone's .gnus
+(setq gnus-read-active-file 'some)
+
+;; Tree view for groups.
+(add-hook 'gnus-group-mode-hook 'gnus-topic-mode)
+
+;; Threads!  I hate reading un-threaded email -- especially mailing
+;; lists.  This helps a ton!
+(setq gnus-summary-thread-gathering-function
+      'gnus-gather-threads-by-subject)
+
+;; Also, I prefer to see only the top level message.  If a message has
+;; several replies or is part of a thread, only show the first
+;; message.
+;; `gnus-thread-ignore-subject' will ignore the subject and
+;; look at 'In-Reply-To:' and 'References:' headers.
+(setq gnus-thread-hide-subtree t)
+(setq gnus-thread-ignore-subject t)
+
+
+(setq user-mail-address "sylvain.ribstein@gmail.com"
+      user-full-name "Sylvain Ribstein")
+
+(setq mm-text-html-renderer 'w3m) ; OPTIONAL
+
+
+(setq gnus-use-correct-string-widths nil)
+
+
+(setq gnus-summary-line-format "%U%R%z %4i %([%4L: %-25,25f]%) %I%s\n")
+(setq gnus-summary-same-subject "-||-")
+
+(eval-after-load 'gnus-topic
+  '(progn
+     (setq gnus-message-archive-group
+           '((format-time-string "sent.%Y")))
+     (setq gnus-server-alist
+           '(("Archivo" nnfolder "archive"
+              (nnfolder-directory "~/Mail/archive")
+              (nnfolder-active-file "~/Mail/archive/active")
+              (nnfolder-get-new-mail nil)
+              (nnfolder-inhibit-expiry t))))
+
+     ;; "Gnus" is the root folder, and there are three mail accounts,
+     ;; "misc", "hotmail", "gmail"
+     (setq gnus-topic-topology
+           '(("Mail" visible)
+             (("Gmail" visible)
+              (("New" visible))
+              (("Perso" visible))
+              (("Admin" visible))
+              (("Pro" visible ))
+              (("Other" invisible ))
+              (("Misc" invisible ))
+              (("Junk" invisible)))
+             (("Outlook" invisible))
+             ))
+
+     ;; each topic corresponds to a public imap folder
+     (setq gnus-topic-alist
+           '(
+             ("New"
+              "INBOX"
+              )
+             ("Perso"
+              "Personal"
+              "Personal/A mí mismo"
+              "Personal/Amigo"
+              "Personal/Familia"
+              "Personal/Volim"
+              "Personal/Activitad"
+              "Personal/Activitad/Désarçonnée"
+              )
+             ("Admin"
+              "Administrativo"
+              "Administrativo/Apartamento"
+              "Administrativo/Billete"
+              "Administrativo/Billete/Actividad"
+              "Administrativo/Billete/Avión"
+              "Administrativo/Billete/Avión"
+              "Administrativo/Billete/Train"
+              "Administrativo/Caf"
+              "Administrativo/Compra"
+              "Administrativo/Compra/Google play"
+              "Administrativo/Contrato/"
+              "Administrativo/Donacion de sangre"
+              "Administrativo/Salud"
+              )
+             ("Pro"
+              "Profesional"
+              "Profesional/Precious Plastic"
+              "Profesional/Scout"
+              "Profesional/Trabajo"
+              "Profesional/Universidad"
+              "Profesional/Voluntario"
+              )
+             ("Other"
+              "Misceláneo"
+              "Freecycle"
+              )
+             ("Misc"; the key of topic
+              "nnset.2019"
+              "nndraft:drafts"
+              "[Gmail]"
+              "[Gmail]/Enviados"
+              "[Gmail]/Borradores"
+              "nnfolder+archive:sent.2019"
+              )
+             ("Junk"; the key of topic
+              "[Gmail]/Spam"
+              "[Gmail]/Papelera"
+              "nnimap+outlook:Junk"
+              "nnimap+outlook:Corbeille"
+              "nnimap+outlook:Deleted"
+              )
+             ("Outlook"
+              "nnimap+outlook:Inbox"
+              "nnimap+outlook:proxGroup"
+              "nnimap+outlook:POP"
+              "nnimap+outlook:bordel a trier/bobcanard@hotmail.fr"
+              "nnimap+outlook:Drafts"
+              "nnimap+outlook:Sent"
+              "nnimap+outlook:Archivo"
+              "nnimap+outlook:Archivo/bobcanard@hotmail.fr"
+              "nnimap+outlook:Deleted/bordel a trier"
+              "nnimap+outlook:église"
+              "nnimap+outlook:Archive"
+              "nnimap+outlook:Envoyés"
+              "nnimap+outlook:bordel a trier"
+              "nnimap+outlook:famillia"
+              "nnimap+outlook:éclais"
+              "nnimap+outlook:Notes"
+              "nnimap+outlook:Outbox"
+              "nnimap+outlook:famille"
+
+              )
+             ))))
+(setq nnmail-split-methods 'nnmail-split-fancy)
+
+
+(setq nnimap-split-fancy
+      '(|("Subject" ".*freecycle.*" "Freecycle")
+         "INBOX"))
