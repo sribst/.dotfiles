7d3b4169578ab611a406013ccf01bbfae62d25ff
sylvain.ribstein <sylvain.ribstein@movesol.com>
2019-07-09 11:02:29 +0200

    adding polybar

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+killall polybar
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+      # MONITOR=$m polybar topbar    -c ~/.config/polybar/topbar    &
+      MONITOR=$m polybar bottombar -c ~/.config/polybar/bottombar &
+  done
+else
+    # polybar topbar    -c ~/.config/polybar/topbar    &
+    polybar bottombar -c ~/.config/polybar/bottombar
+fi
